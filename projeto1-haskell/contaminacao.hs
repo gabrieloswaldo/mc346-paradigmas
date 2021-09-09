96902d5b073a3fc2d1db6453c16077cf6d16636b
gabrieloswaldo <gabriel.oswaldo99@gmail.com>
2021-09-08 23:38:45 -0300

    Implement dijkstra algorithm and helper methods

--- projeto1-haskell/contaminacao.hs
+++ projeto1-haskell/contaminacao.hs
@@ -1,30 +1,101 @@
-import Data.List as DL
-import Data.Map.Strict as Map
+{-
+  MC346 - Projeto 1 - Haskell
+
+  Alunos:
+    Gabriel Henrique Rosa Oswaldo - 172185
+    Bruno Rosano Laporte Ambrosio - 195141
+-}
+
+import qualified Data.List as DL
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Text.Printf (printf)
 
 
--- Setup
+-- Definição dos tipos utilizados
+type Node = String
+type Pred = Node
+type Dist = Float
+type Weight = Float
+type Visited = Bool
+type NodeInfo = (Dist, Pred, Weight, Visited)
+type DNode = (Node, NodeInfo)
+type DNodeMap = Map Node NodeInfo
+type Edge = (Node, Weight)
+type Graph = Map Node [Edge]
 
-setup :: [[String]] -> Map String [(String, String)]
-setup l = Map.fromListWith (++) $ Prelude.map (\[a, b, c] -> (a, [(b, c)])) l
+
+-- Algoritmo de Dijkstra, e métodos auxiliares, para achar 
+-- a árvore de caminhos mínimos em um grafo não direcionado 
+-- com pesos não negativos.
+dijkstra :: Graph -> Node -> DNodeMap
+dijkstra graph start = dijkstra' queue Map.empty
+  where queue = initSingleSrc graph start
+        dijkstra' q tree
+          | Map.size tree == Map.size graph = tree
+          | otherwise = dijkstra' newQ $ Map.insert u uInfo tree
+          where nodeU = extractMinQ q
+                u = fst nodeU
+                uInfo = snd nodeU
+                newQ = relax graph nodeU $ deleteMinQ q u
+
+initSingleSrc :: Graph -> Node -> DNodeMap
+initSingleSrc graph start = Map.fromList $ Prelude.map initSingleSrc' $ Map.keys graph
+  where initSingleSrc' node = if (node == start) 
+                              then (node, (0, "nil", 0, False))
+                              else (node, (1/0, "nil", 0, False))
+
+extractMinQ :: DNodeMap -> DNode
+extractMinQ q = DL.minimumBy (\(_,(du, _, _, _)) (_,(dv, _, _, _)) -> compare du dv) $ notVisited $ Map.toList q
+  where notVisited = Prelude.filter (\(_,(_, _, _, e)) -> e == False)
+
+deleteMinQ :: DNodeMap -> Node -> DNodeMap
+deleteMinQ q minKey = Map.insert minKey (softDelete $ Map.elemAt (getMaybe $ Map.lookupIndex minKey q) q) q
+  where softDelete (_, (du, pred, wuv, _)) = (du, pred, wuv, True)
+
+relax :: Graph -> DNode -> DNodeMap -> DNodeMap
+relax graph node queue = relax' adj queue
+  where u = fst node
+        du = fst' $ snd node
+        fst' (a,_,_,_) = a
+        adj = getMaybe $ Map.lookup u graph
+        relax' [] q = q
+        relax' ((v, wuv):es) q
+          | dv > du + wuv = relax' es $ Map.insert v (du + wuv, u, wuv, visited) q
+          | otherwise = relax' es q
+          where nodeV = getMaybe $ Map.lookup v q
+                dv = fst' nodeV
+                visited = (\(_,_,_,d) -> d) nodeV
 
 
--- Métodos para processar o input
+-- Métodos auxiliares
+getMaybe :: Maybe a -> a
+getMaybe = fromMaybe (error "Erro ao extrair valor maybe")
 
-getLines :: [[String]] -> [[String]]
-getLines l = addLines $ DL.takeWhile (not . DL.null) l
-  where addLines l = l ++ (Prelude.map (\[a, b, c] -> [b, a, c]) l)
+showResult :: DNodeMap -> IO ()
+showResult tree = printf "%.2f\n" $ Map.foldl (\acc (_, _, wuv, _) -> acc + wuv) 0 tree
 
-breakInput :: String -> [[String]]
-breakInput input = Prelude.map words $ lines input
 
+-- Setup do grafo utilizando um Map
+buildGraph :: [[String]] -> Graph
+buildGraph l = Map.fromListWith (++) $ Prelude.map (\[a, b, c] -> (a, [(b, (/) 1 $ read c :: Float)])) l
+
+
+-- Método para processar o input
 processInput :: String -> ([[String]], String)
-processInput input = let brokenInput = breakInput input
-                  in (getLines brokenInput, head $ last brokenInput)
+processInput input = (getLines brokenInput, head $ last brokenInput)
+  where brokenInput = breakInput input
+        breakInput inpt = Prelude.map words $ lines inpt
+        getLines l = addLines $ DL.takeWhile (not . DL.null) l
+        addLines l = l ++ (Prelude.map (\[a, b, c] -> [b, a, c]) l)
 
 
 -- Main
-
 main :: IO ()
 main = do
   input <- getContents
-  putStrLn (show $ setup $ fst $ processInput input)
+  let processedInput = processInput input
+  let startNode = snd processedInput
+  let graph = buildGraph $ fst processedInput
+  showResult $ dijkstra graph startNode
